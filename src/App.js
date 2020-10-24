4081e84ebd9062663e59ae70e21efbe94c562310
Prince Patel <46424040+20prince12@users.noreply.github.com>
2020-10-24 08:36:24 -0700

    Add Dockerfile and docker-compose.yml

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h> Welcome</h>
         <a
           className="App-link"
           href="https://reactjs.org"
