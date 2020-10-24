0bf9de6d2dbdd7d65b3a69fa87be0fd6d2148dd9
Prince Patel <46424040+20prince12@users.noreply.github.com>
2020-10-24 09:46:15 -0700

    Remove h tag to fix build

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h> Welcome</h>
         <a
           className="App-link"
           href="https://reactjs.org"
